b2d03ce52f851899e6b39ea22880c61b3f08a6b8
Laura Royle <lauraamie.royle@gmail.com>
2019-01-23 13:44:37 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,9 +23,9 @@ def end_game(card_total)
 end
 
 def initial_round
+  return deal_card + deal_card
   card_total = deal_card + deal_card
   return display_card_total(card_total)
-  return card_total
 end
 
 def hit?(currentcard_total)
